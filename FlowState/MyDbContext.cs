f67a886b0cfe3ffec99bbb537e1f3d0953791ce9
Konkz7 <amara03@live.co.uk>
2026-05-27 11:54:02 +0100

    setup in memory db with basic Task MVC

---
+++ FlowState/MyDbContext.cs
@@ -0,0 +1,18 @@
+﻿using FlowState.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+
+namespace FlowState
+{
+    public class MyDbContext : DbContext
+    {
+        
+        public DbSet<ToDoTask> Tasks { get; set; }
+
+        public MyDbContext(DbContextOptions<MyDbContext> options)
+        : base(options)
+        {
+        }
+    }
+}
--- FlowState/Program.cs
+++ FlowState/Program.cs
@@ -1,13 +1,31 @@
+using FlowState;
+using FlowState.Services;
+using FlowState.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddScoped<TaskService>();
+builder.Services.AddScoped<TaskRepo>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<MyDbContext>(options =>
+   options.UseInMemoryDatabase("TestDb"));
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<MyDbContext>();
+    db.Database.EnsureCreated();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
